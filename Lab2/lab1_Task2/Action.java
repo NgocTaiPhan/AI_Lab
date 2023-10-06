efc30a69974121e470484d2b127bfb8a8f208725
OzoriTA <ngoctaiphan.cv@gmail.com>
2023-10-06 14:45:48 +0700

    Hoàn thành lab1

--- Lab2/lab1_Task2/Action.java
+++
@@ -1,5 +0,0 @@
-package lab1_Task2; 
-
-public abstract class Action {
-	public abstract boolean isNoOp();
-}
--- Lab2/lab1_Task2/NoOpAction.java
+++
@@ -1,9 +0,0 @@
-package lab1_Task2; 
-
-public class NoOpAction extends Action {
-	public static final NoOpAction NO_OP = new NoOpAction();
-
-	public boolean isNoOp() {
-		return true;
-	}
-}
